7f805bf927f3660fa6b79346fa36f7e074778412
Brett Kinnamon <me@brettk.dev>
2022-07-26 08:32:04 -0400

    reset neovim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,17 +1,2 @@
-require('plugins')
-require('gruvbox-config')
-require('nvim-cmp-config')
-require('null-ls-config')
-
-vim.api.nvim_create_autocmd("BufWritePre", {
-		pattern = "*",
-		callback = function(args)
-			vim.lsp.buf.formatting_sync()
-		end,
-		desc = "Format with LSP on save.",
-	})
-
-vim.wo.number = true
-vim.opt.scrolloff = 10
-
-vim.opt.expandtab = true
+require("brettk.set")
+require("plugins")
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,32 +1,6 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
-
--- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function()
   use 'wbthomason/packer.nvim'
-  -- use 'w0rp/ale'
-  use 'sheerun/vim-polyglot'
-  use 'tpope/vim-fugitive'
-  use 'tpope/vim-surround'
-  use 'tpope/vim-commentary'
-  use 'tpope/vim-abolish'
-  use 'altercation/vim-colors-solarized'
-  use 'mattn/emmet-vim'
-  -- use 'ycm-core/YouCompleteMe'
-  -- use 'Quramy/tsuquyomi'
-  -- use 'Quramy/tsuquyomi-vue'
-  use 'neovim/nvim-lspconfig'
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'saadparwaiz1/cmp_luasnip'
-  use 'L3MON4D3/LuaSnip'
-  use 'jose-elias-alvarez/null-ls.nvim'
-  -- use 'MunifTanjim/prettier.nvim'
-  use 'morhetz/gruvbox'
-
-  use {
-    'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' },
-    config = function() require('gitsigns').setup() end
-  }
+  use 'folke/tokyonight.nvim'
 end)
